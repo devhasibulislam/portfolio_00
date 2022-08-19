506f4bd36c10f72c22712419143103b1d1331f4a
hasibulislam999 <hasib143sl@gmail.com>
2022-08-20 00:26:02 +0600

    footer integrated

---
+++ src/components/footer/Footer.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import style from "./footer.module.css";
+import { Row, Col } from "react-bootstrap";
+export const Footer = () => {
+  return (
+    <footer className={style.footer}>
+      <main className={style.footer_content}>
+        <Row>
+          <Col
+            lg={5}
+            md={5}
+            className=" text-center text-sm-start order-3 order-lg-0 order-md-0 d-flex justify-content-center justify-content-lg-start justify-content-md-start"
+          >
+            <p className={style.footer_text + " small"}>
+              © {new Date().getFullYear()} Hasib. All Rights Reserved.
+            </p>
+          </Col>
+          <Col lg={2} md={2} className="d-flex justify-content-center">
+            <img
+              src="/media/logos/lalogobw.svg"
+              className={style.footer_brand + " mb-3 mb-lg-0 mb-md-0"}
+            />
+          </Col>
+          <Col
+            lg={5}
+            md={5}
+            className="d-flex justify-content-center justify-content-lg-end justify-content-md-end text-center text-sm-start"
+          >
+            <p className={style.footer_text + " small"}>
+              Designed & Developed by Hasibul Islam
+            </p>
+          </Col>
+        </Row>
+      </main>
+    </footer>
+  );
+};
