cef7598a188b73e84843fe9fe221965fcd746026
hasibulislam999 <hasib143sl@gmail.com>
2022-09-05 18:30:27 +0600

    project myth added

--- src/components/myProjects.tsx
+++ src/components/myProjects.tsx
@@ -3,7 +3,7 @@ export const myProjects = [
     title: "Red Onion",
     description:
     "Instagram clone built using Next JS, Typescript, and TailwindCSS. This web application was built solely for research purposes and not for any malicious intent.",
-    img: "/media/images/redonion.png",
+    img: "/media/images/red-onion.jpeg",
     tags: ["ReactJS", "React Bootstrap", "Firebase"],
     cta: "View site",
     url: "https://red-onion-clinet.vercel.app/home/breakfast",
@@ -13,21 +13,21 @@ export const myProjects = [
     title: "Logo Tech",
     description:
       "Enhance your logo with our technology that helps you to get appropriate logo for your business. Which seamlessly grab attention of vendors.",
-    img: "/media/images/logotech.png",
+    img: "/media/images/logo-tech.jpg",
     tags: ["ReactJS", "Tailwind CSS", "Firebase"],
     cta: "Visit site",
     url: "https://logo-tech.vercel.app/",
-    left: true,
+    left: false,
   },
   {
     title: "Creative Agency",
     description:
       "Recreate agencies with their user or local policy that helps them to identify with proper optimistic agency to threshold and convey user with trust.",
-    img: "/media/images/creativeagency.png",
+    img: "/media/images/creative-agency.png",
     tags: ["ReactJS", "Express", "MongoDB", "Firebase", "React Bootstrap"],
     cta: "View site",
     url: "https://creative-agency-43bbc.web.app/",
-    left: false,
+    left: true,
   },
   {
     title: "Luxury Living",
@@ -37,6 +37,106 @@ export const myProjects = [
     tags: ["NextJS", "Express", "MongoDB", "Firebase", "DaisyUI"],
     cta: "View site",
     url: "https://llc-luxury-living-client.vercel.app/",
+    left: false,
+  },
+  {
+    title: "Doctors Portal [Deprecated]",
+    description:
+      "It is a doctors portal appointment based web application or website which simply construct with all need with a doctor and with a patient like making realtime appointment with a doctor.",
+    img: "/media/images/doctors-portal.jpeg",
+    tags: ["ReactJS", "Express", "MongoDB", "Firebase", "DaisyUI"],
+    cta: "View site",
+    url: "https://doctors-portal-d1164.web.app/",
+    left: true,
+  },
+  {
+    title: "Arsha",
+    description:
+      "It's solution based website which provide multi-types solutions in business to scale it as a next level concern. But this is a static website which radially used for display as a project but as taking ideas it's literally best.",
+    img: "/media/images/arsha.png",
+    tags: ["HTML5", "CSS3", "Bootstrap 5"],
+    cta: "View site",
+    url: "https://hasibulislam999.github.io/demo-arsha/",
+    left: false,
+  },
+  {
+    title: "Bethany",
+    description:
+      "It's an online attendence blooming website which institute used for presence of their own to engage with greet valley. Get obsolate token wheather necessery.Used to display the samplistic idea as own.",
+    img: "/media/images/bethany.png",
+    tags: ["HTML5", "CSS3", "Bootstrap 5"],
+    cta: "View site",
+    url: "https://hasibulislam999.github.io/demo-bethany/",
+    left: true,
+  },
+  {
+    title: "Moderna",
+    description:
+      "It's an online attendence blooming website which institute used for presence of their own to engage with greet valley. Get obsolate token wheather necessery.Used to display the samplistic idea as own.",
+    img: "/media/images/moderna.png",
+    tags: ["HTML5", "CSS3", "Bootstrap 5"],
+    cta: "View site",
+    url: "https://hasibulislam999.github.io/demo-mordana/",
+    left: false,
+  },
+  {
+    title: "The Event",
+    description:
+      "It's an event organizing or event insperation website. Which can obey an event and represent it's sweet and anguler options which although can retain the semantics of any location.",
+    img: "/media/images/the-event.png",
+    tags: ["HTML5", "CSS3", "Bootstrap 5"],
+    cta: "View site",
+    url: "https://hasibulislam999.github.io/demo-theEvent/",
+    left: true,
+  },
+  {
+    title: "Travel Guru",
+    description:
+      "It's a travel management minimal website that not gonna used by professional. Although it is hobby testing site just figure out the functionality to convey with localStorage as a sense and conduct.",
+    img: "/media/images/travel-guru.jpeg",
+    tags: ["ReactJS", "Material UI", "Firebase", "React-router"],
+    cta: "View site",
+    url: "https://travel-guru-a71d0.web.app/",
+    left: false,
+  },
+  {
+    title: "TSC [Deprecated]",
+    description:
+      "It's a full database website base on teacher student center. Which concludes an attendance type cemetery. That allows student to add notes like do in note app and also allows to do minimally for a teacher to add or remove student.",
+    img: "/media/images/teacher-student-center.jpg",
+    tags: ["ReactJS", "Tailwind CSS", "HTML-TO-Excel", "Hook-Form", "ExpressJS", "MongoDB", "NodeJS"],
+    cta: "View site",
+    url: "https://tsc-client-coral.vercel.app/",
+    left: true,
+  },
+  {
+    title: "Node Chat Application [Deprecated]",
+    description:
+      "A full stack project which is now deprecated. And not able to give other way support right now and not eligible to have best out of it. As a tough project it's technology getting old and wisher support getting deprecated therefore no running state on it.",
+    img: "/media/images/node-chat-app.png",
+    tags: ["NodeJS", "JWT", "MongoDB", "EJS"],
+    cta: "View Project",
+    url: "https://github.com/hasibulislam999/node-chat-application",
+    left: false,
+  },
+  {
+    title: "Create random user API",
+    description:
+      "A full stack project which is now deprecated. And not able to give other way support right now and not eligible to have best out of it. As a tough project it's technology getting old and wisher support getting deprecated therefore no running state on it.",
+    img: "/media/images/random-user-api.png",
+    tags: ["NodeJS", "ES6", "LOCAL DB", "HTML5", "ExpressJS"],
+    cta: "View Project",
+    url: "https://github.com/hasibulislam999/random-user-api",
+    left: true,
+  },
+  {
+    title: "Directory of books",
+    description:
+      "A full stack project which is now deprecated. And not able to give other way support right now and not eligible to have best out of it. As a tough project it's technology getting old and wisher support getting deprecated therefore no running state on it.",
+    img: "/media/images/directory-books.png",
+    tags: ["NodeJS", "ES6", "LOCAL DB", "HTML5", "ExpressJS"],
+    cta: "View Project",
+    url: "https://github.com/hasibulislam999/random-user-api",
     left: false,
   },
 ];
